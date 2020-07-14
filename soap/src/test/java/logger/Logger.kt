5f3b63fae4592e00dbeba3acd151f88fe7f91158
snromanov <stanislav.r@alyce.com>
2020-07-14 10:15:10 +0300

    Change project

---
+++ soap/src/test/java/logger/Logger.kt
@@ -0,0 +1,6 @@
+package logger
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+val log: Logger = LoggerFactory.getLogger("SoapLogger")
--- soap/src/test/kotlin/calculator/TestKt.kt
+++ soap/src/test/kotlin/calculator/TestKt.kt
@@ -2,18 +2,28 @@ package calculator
 
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import org.tempuri.Calculator
 import java.net.URI
+import java.util.stream.Stream
 
 class TestKt {
+    private val calc = Calculator(URI.create("http://www.dneonline.com/calculator.asmx?wsdl").toURL()).calculatorSoap12
 
     @Test
     fun `check add`() {
-        Calculator(
-            URI.create("http://www.dneonline.com/calculator.asmx?wsdl").toURL()
-        ).calculatorSoap12.also {
+        calc.add(1, 3).shouldBe(4)
+    }
 
-            it.add(1, 3).shouldBe(4)
-        }
+    @ParameterizedTest
+    @MethodSource("intProvider")
+    fun `Test with custom arguments provider`(argument: Int) {
+        calc.add(argument, argument).shouldBe(Math.addExact(argument, argument))
+    }
+
+    companion object {
+        @JvmStatic
+        fun intProvider(): Stream<Int> = Stream.of(3, 5, 6, 7)
     }
 }
