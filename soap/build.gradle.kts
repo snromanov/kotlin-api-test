17e4cb34b083ca3a6b6d3c88dc6b9b9a6318bf29
snromanov <stanislav.r@alyce.com>
2020-07-14 10:22:45 +0300

    some change

--- soap/build.gradle.kts
+++ soap/build.gradle.kts
@@ -5,11 +5,10 @@ import Build_gradle.Versions.jaxbCoreVersion
 import Build_gradle.Versions.jaxbImplVersion
 import Build_gradle.Versions.jaxbRuntimeVerison
 import Build_gradle.Versions.jsr181Version
-import no.nils.wsdl2java.Wsdl2JavaTask
 
 plugins {
     `common-library`
-    id("no.nils.wsdl2java") version "0.8"
+    id("no.nils.wsdl2java") version "0.12"
 }
 
 group = "com.integration.testing"
@@ -21,11 +20,11 @@ object Versions {
     const val javaxwsVersion = "2.3.1"
     const val jaxbRuntimeVerison = "2.3.0.1"
     const val jsr181Version = "1.0-MR1"
-    const val jaxbImplVersion = "2.3.1"
+    const val jaxbImplVersion = "2.3.3"
     const val jaxbCoreVersion = "2.3.0.1"
 }
 
-dependencies() {
+dependencies {
     // enable extension support for wsdl2java
     wsdl2java("org.jvnet.jaxb2_commons:jaxb2-basics:$jaxb2CommonsVersion")
     wsdl2java("org.jvnet.jaxb2_commons:jaxb2-basics-ant:$jaxb2CommonsVersion")
@@ -36,30 +35,19 @@ dependencies() {
     wsdl2java("org.glassfish.jaxb:jaxb-runtime:$jaxbRuntimeVerison")
     wsdl2java("javax.xml.ws:jaxws-api:$javaxwsVersion")
     wsdl2java("javax.jws:jsr181-api:$jsr181Version")
+    implementation("javax.xml.ws:jaxws-api:$javaxwsVersion")
+    implementation("com.sun.xml.ws:rt:$javaxwsVersion") {
+        exclude(group = "org.glassfish.jaxb", module = "txw2")
+    }
+    implementation("org.glassfish.jaxb:txw2:$jaxbImplVersion")
     implementation ("org.jvnet.jaxb2_commons:jaxb2-basics-runtime:$jaxb2CommonsVersion")
 }
 
-tasks {
-    withType<Wsdl2JavaTask> {
-        wsdlsToGenerate = arrayListOf(
-            arrayListOf(
-                "-xjc-Xequals",
-                "-xjc-XhashCode",
-                "-xjc-XtoString",
-                "$projectDir/src/main/resources/import.wsdl"
-            )
-        )
-    }
-}
-val wsdl2java: Wsdl2JavaTask by tasks
-sourceSets {
-    getByName("main") {
-        java.setSrcDirs(java.srcDirs + wsdl2java.generatedWsdlDir)
-    }
-}
-afterEvaluate {
-    tasks.forEach {
-        if (it.name == "kaptKotlin" || it.name == "compileKotlin")
-            it.dependsOn(wsdl2java)
-    }
+wsdl2java {
+    wsdlDir = file("$projectDir/src/main/wsdl")
+    wsdlsToGenerate = listOf(
+        listOf( "-xjc-Xequals",
+            "-xjc-XhashCode",
+            "-xjc-XtoString", "$wsdlDir/calculator.wsdl")
+    )
 }
